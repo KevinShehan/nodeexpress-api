7648e0c9590f3fe0e7c98875d9379e14bbc7590d
Kevin <kevin.shehan30@gmail.com>
2024-11-11 23:38:44 +0530

    mongodb connected

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 const express = require('express');
+const mongoose= require('mongoose');
 const app = express();
+
+const port = 3000;
+app.listen(port,() => {
+    console.log(`Server is running on port ${port}`);
+});
+
+
+mongoose.connect('mongodb://localhost:27017/nodeapi')
+.then(() => {
+    console.log("MongoDB Connected Succesfully")
+})
+.catch(()=>{
+    console.log("MongoDB Connection Failed")
+})
+
+app.get('/',(req,res)=>{
+res.send("Hello world! from NODE API");
+});
+
