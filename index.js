80a38fc00994dce5c2d5a7e9976ab7f876c9d47d
Kevin <kevin.shehan30@gmail.com>
2024-11-12 00:06:49 +0530

    post method added

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
 const express = require('express');
-const mongoose= require('mongoose');
+const mongoose = require('mongoose');
 const Product = require('./models/product.model.js');
 const app = express();
 
 const port = 3000;
-app.listen(port,() => {
+app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
 
 mongoose.connect('mongodb://localhost:27017/nodeapi')
-.then(() => {
-    console.log("MongoDB Connected Succesfully");
-})
-.catch(()=>{
-    console.log("MongoDB Connection Failed");
-})
+    .then(() => {
+        console.log("MongoDB Connected Succesfully");
+    })
+    .catch(() => {
+        console.log("MongoDB Connection Failed");
+    })
 
-app.get('/',(req,res)=>{
-res.send("Hello world! from NODE API");
+app.get('/', (req, res) => {
+    res.send("Hello world! from NODE API");
+});
+
+app.post('api/products', (req, res) => {
+    res.send("Data Recieved");
+
 });
 
