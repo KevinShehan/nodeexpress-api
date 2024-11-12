a45cf06f642f6da6746eec7158839ec2e9a1aef4
Kevin <kevin.shehan30@gmail.com>
2024-11-12 21:19:35 +0530

    dotenv added environment dclare

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Product = require('./models/product.model.js');
 const app = express();
 app.use(express.json());
 
-const port = 3000;
+const port = process.env.PORT ||3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
