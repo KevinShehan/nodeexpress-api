a3e191716d80569f7c9b681967302913e86e941d
Kevin <kevin.shehan30@gmail.com>
2024-11-12 12:43:37 +0530

    formated code indentation

--- index.js
+++ index.js
@@ -22,24 +22,24 @@ app.get('/', (req, res) => {
     res.send("Hello world! from NODE API");
 });
 
-app.post('/api/products', async(req, res) => {
+app.post('/api/products', async (req, res) => {
     // res.send("Data Recieved");
     // console.log(req.body);
-    try{
+    try {
         const product = await Product.create(req.body);
         res.statusCode(201).json(product);
     }
-    catch(err){
-        res.status(500).json({message: err.message});
+    catch (err) {
+        res.status(500).json({ message: err.message });
     }
 });
 
-app.get('/api/products',async(req,res)=>{
-try{
-    const products = await Product.find({});
-    res.status(200).json(products);
-}
-catch(error){
-    res.status(500).json({message:error.message});
-}
+app.get('/api/products', async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products);
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 });
