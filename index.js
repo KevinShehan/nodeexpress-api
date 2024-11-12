1b57ff57b33dd0fd388191249c928809bcc19a24
Kevin <kevin.shehan30@gmail.com>
2024-11-12 12:39:42 +0530

    get all api

--- index.js
+++ index.js
@@ -30,7 +30,16 @@ app.post('/api/products', async(req, res) => {
         res.statusCode(201).json(product);
     }
     catch(err){
-        res.status(500).json({message: err.message});
+        res.s{tatus(500).json({message: err.message});
     }
 });
 
+app.get('/api/products',async(req,res)=>{
+try{
+    const products = await Product.find({});
+    res.status(200).json(products);
+}
+catch(error){
+    res.status(500).json({message:error.message});
+}
+});
