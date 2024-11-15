6012380a3ac3fd2b3bd1dd5e14843732f4f5267f
Kevin <kevin.shehan30@gmail.com>
2024-11-15 20:29:09 +0530

    body cors dependency added

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const Product = require('./models/product.model.js');
 const productRoute = require('./routes/product.route.js'); 
 require('dotenv').config();
 const app = express();
-
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
