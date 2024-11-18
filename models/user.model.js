810a1b37a064d70c36145fcc0c7e09dd12be4a7c
Kevin <kevin.shehan30@gmail.com>
2024-11-18 11:52:48 +0530

    Hash password before saving

--- models/user.model.js
+++ models/user.model.js
@@ -1,16 +1,24 @@
 const mongoose = require('mongoose');
 const UserSchema = mongoose.Schema({
     name: {
-        type:String,
-        required:true
+        type: String,
+        required: true
     },
     username: {
         type: String,
-        required:true,
-        unique:true
+        required: true,
+        unique: true
     },
     password: String,
 });
 
+// Hash password before saving
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  });
+  
 const User = mongoose.model('User', UserSchema);
 module.exports = User;
